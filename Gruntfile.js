b403cd6a8a8eafe1c47b9092d9a1f1ae3711e262
i5on9i <gaedduck@gmail.com>
2015-12-10 12:46:12 +0900

    - rename the task

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
   // deploy
   grunt.registerTask('jsx_deploy', ['coffee:compile_need_jsx', 'babel:jsx', 'concat:build_from_src_out', 'copy:server_deploy_coffee']);
   grunt.registerTask('bower_deploy', ['bower_concat:all', 'copy:server_deploy_bowerconcat']);  
-  grunt.registerTask('sass-deploy', ['sass:main', 'copy:server_deploy_css']);
-  grunt.registerTask('html-deploy', ['copy:to_build_html', 'copy:server_deploy_html']);
+  grunt.registerTask('sass_deploy', ['sass:main', 'copy:server_deploy_css']);
+  grunt.registerTask('html_deploy', ['copy:to_build_html', 'copy:server_deploy_html']);
   
 };
